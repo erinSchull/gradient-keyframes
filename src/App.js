e60f2a5f1280a4b0d6967bde4327958e504d8427
Erin Schull <erin.schull@gmail.com>
2018-04-11 20:23:20 -0600

    first commit pretty much done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Header Text</h1>
+        <div className="body">
+          <h1 className="title" >Playing with hover animations</h1>
+          <section className="my-box" >Check me out!</section>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
