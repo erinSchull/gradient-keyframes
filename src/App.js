b24999f629479ffb0b1e422844bb56a9220a2c25
Erin Schull <erin.schull@gmail.com>
2018-04-21 00:59:41 -0600

    added picture and widget sections

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Header Text</h1>
+        <div className="header" >
+          <h1>Header Text</h1>
+          <section className="my-widget" >A widget on the edge of the green </section>
+        </div>
         <div className="body">
           <h1 className="title" >Playing with hover animations</h1>
           <section className="my-box" >Check me out!</section>
+          <br />
+          <h2>A title before some pictures</h2>
+          <div className="pictures" >
+            <section className="image1" >Image 1</section>
+            <section className="image2" >Image 2</section>
+            <section className="image3" >Image 3</section>
+            <section className="image4" >Image 4</section>
+            <section className="image5" >Image 5</section>
+            <section className="image6" >Image 6</section>
+          </div>
         </div>
       </div>
     );
