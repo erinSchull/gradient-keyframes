00d38c9227f93d7a29847e84b1bfd81dbe200040
Erin Schull <erin.schull@gmail.com>
2018-06-17 17:51:58 -0600

    images added and implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import num1 from './Images/one.png';
+import num2 from './Images/two.png';
+import num3 from './Images/three.png';
+import num4 from './Images/four.png';
+import num5 from './Images/five.png';
+import num6 from './Images/six.png';
 
 class App extends Component {
   render() {
@@ -15,12 +21,24 @@ class App extends Component {
           <br />
           <h2>A title before some pictures</h2>
           <div className="pictures" >
-            <section className="image1" >Image 1</section>
-            <section className="image2" >Image 2</section>
-            <section className="image3" >Image 3</section>
-            <section className="image4" >Image 4</section>
-            <section className="image5" >Image 5</section>
-            <section className="image6" >Image 6</section>
+            <section className="image1" >Image 1
+            <img src={num1} alt='num1' className="imgnum1" />
+            </section>
+            <section className="image2" >Image 2
+            <img src={num2} alt='num2' className="imgnum2" />
+            </section>
+            <section className="image3" >Image 3
+            <img src={num3} alt='num3' className="imgnum3" />
+            </section>
+            <section className="image4" >Image 4
+            <img src={num4} alt='num4' className="imgnum4" />
+            </section>
+            <section className="image5" >Image 5
+            <img src={num5} alt='num5' className="imgnum5" />
+            </section>
+            <section className="image6" >Image 6
+            <img src={num6} alt='num6' className="imgnum6" />
+            </section>
           </div>
         </div>
       </div>
